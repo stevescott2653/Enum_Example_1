2d48be8cb4e4f21712899d63d0ec5fdefbf2720a
Steven Scott <stevescott2653@gmail.com>
2019-09-25 16:15:00 -0400

    Add files via upload

---
+++ Enum_Example_1/Enum_Example_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Example_1
+{
+    class Program
+    {
+        public enum Season { WINTER, SPRING, SUMMER, FALL }
+        static void Main(string[] args)
+        {
+            int x = (int)Season.SPRING;
+            int y = (int)Season.FALL;
+            Console.WriteLine("WINTER = {0}", x);
+            Console.WriteLine("SUMMER = {0}", y);
+        }
+    }
+}
